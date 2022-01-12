60f54913189ef61e474016c1617414d6bb0d6b1c
Sophia9156 <suhyun9156@gmail.com>
2022-01-12 14:32:31 +0900

    220112-1

--- js/cart.js
+++ js/cart.js
@@ -9,3 +9,21 @@ if(localStorage.buyquantity){
   $('.empty').removeClass('active');
   $('.filled').addClass('active');
 }
+
+// 전체상품 체크
+selectAll.checked;
+selectAll.onclick = function(){
+  selectOne.checked = this.checked;
+};
+
+
+// 장바구니에 상품 localStorage로 추가하기
+let dataChange = function(){
+  $.ajax({
+    url: 'js/data.json',
+    success: function(data){
+
+    }
+  })
+}
+dataChange();
